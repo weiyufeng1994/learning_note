287523819ba796d2622d2910df22d49afd517834
Shimazaki Nanase <39324454+weiyufeng1994@users.noreply.github.com>
2021-04-27 00:58:38 +0900

    Add files via upload

---
+++ python_work/print_t.py
@@ -0,0 +1,3 @@
+print("\tPython")
+print("Languages:\n\tPython\n\tC\n\tJavascript")
+print("Python")
---
+++ python_work/strip.py
@@ -0,0 +1,5 @@
+favorite_language=' python '
+print(favorite_language)
+print(favorite_language.rstrip())
+print(favorite_language.lstrip())
+print(favorite_language.strip())
