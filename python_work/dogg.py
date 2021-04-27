af125848571ae5d7a1428f798ede2a1332587623
Shimazaki Nanase <39324454+weiyufeng1994@users.noreply.github.com>
2021-04-27 21:14:42 +0900

    Add files via upload

---
+++ python_work/dogg.py
@@ -0,0 +1,22 @@
+class Dog:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        print(f"{self.name} is now sitting.")
+
+    def roll_over(self):
+        print(f"{self.name} rolled over!")
+
+my_dog = Dog('Willie', 6)
+your_dog = Dog('Lucy', 3)
+
+print(f"My dog's name is {my_dog.name}.")
+print(f"My dog is {my_dog.age} years old.")
+my_dog.sit()
+
+print(f"\nYour dog's name is {your_dog.name}.")
+print(f"Your dog is {your_dog.age} years old.")
+your_dog.sit()
